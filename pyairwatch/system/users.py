d7b8bdebcad4f3ac5ff15c08b9dd8edaaff197e9
Clayton Burlison <git@clburlison.com>
2020-06-08 23:04:32 -0500

    feat: Add create_user & delete user v2 endpoints
    Also updated function names for get_user_by_uuid and update_user_by_uuid

--- pyairwatch/system/users.py
+++ pyairwatch/system/users.py
@@ -22,7 +22,7 @@ class Users(System):
         """
         return System._get(self, path='/users/search', params=kwargs)
 
-    def get_user_by_id(self, uuid):
+    def get_user_by_uuid(self, uuid):
         """
         Returns the enrollment user for a specific uuid using the v2 endpoint.
 
@@ -33,9 +33,49 @@ class Users(System):
         _header = {'Accept': 'application/json;version=2'}
         return System._get(self, header=_header, path=_path)
 
-    def update_user_by_id(self, uuid: str=None, **kwargs):
+    def create_user(self, **kwargs):
         """
-        Update the enrollment user with attributes using the v2 endpoint that supports Access.
+        Create an enrollment user using the v2 endpoint.
+
+        /api/system/users/
+
+        PARAMS:
+            externalId={e559e7df-4ba0-4891-9fcd-8574c1770d34}
+            userName={username}
+            password={password}
+            firstName={firstname}
+            lastName={lastname}
+            displayName={displayname}
+            userPrincipalName={testuser@gandalf.dev}
+            emailAddress={noreply@vmware.com}
+            emailUsername={noreply@vmware.come}
+            phoneNumber={1-111-111-1111}
+            mobileNumber={+1(111)-111-1111}
+            messageType={Email}
+            messageTemplateUuid={53f732a0-3e08-474d-80f4-ff976b8eb698}
+            enrollmentRoleUuid={599b9117-f399-4e60-a96b-cd1f771d5f06}
+            status=true,
+            securityType={directory}     REQUIRED
+            deviceStagingEnabled=false,
+            deviceStagingType={StagingDisabled}
+            organizationGroupUuid={6fbc95c6-3269-4a88-804d-c8db7f479d7f}
+            enrollmentOrganizationGroupUuid={94b1b965-59b9-462c-ad18-4a228f9830dd}
+            customAttribute1={CustomAttribute1}
+            customAttribute2={CustomAttribute2}
+            customAttribute3={CustomAttribute3}
+            customAttribute4={CustomAttribute4}
+            customAttribute5={CustomAttribute5}
+            aadMappingAttribute={e559e7df-4ba0-4891-9fcd-8574c1770d34}
+            department={Sales}
+            employeeIdentifier={12345}
+            costCenter={110)
+        """
+        _header = {'Accept': 'application/json;version=2'}
+        return System._post(self, header=_header, path="/users/", json=kwargs)
+
+    def update_user_by_uuid(self, uuid: str=None, **kwargs):
+        """
+        Update the enrollment user with attributes using the v2 endpoint.
 
         /api/system/users/{uuid}
 
@@ -64,6 +104,17 @@ class Users(System):
         _path = "/users/{}".format(uuid)
         _header = {'Accept': 'application/json;version=2'}
         return System._put(self, path=_path, header=_header, json=kwargs)
+
+    def delete_user_by_id(self, user_id):
+        """
+        Delete the enrollment user by enrollment user uuid with the v2 endpoint.
+
+        :param uuid:
+        :return: API response
+        """
+        _path = '/users/{}'.format(uuid)
+        _header = {'Accept': 'application/json;version=2'}
+        return System._delete(self, header=_header, path=_path)
 
     def delete_user_by_id(self, user_id):
         """
