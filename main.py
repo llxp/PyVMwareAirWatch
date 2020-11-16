cc66b8f35b1f6258ef338d2797eaac7fdba3c94d
Marco Fuchs <marco.fuchs@live.de>
2020-11-16 22:41:39 +0100

    Import pprint
    Import pprint for development purposes.
    pprint generates better structured output for debugging.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from pyairwatch.client import AirWatchAPI
+import pprint
 
 a = AirWatchAPI(env='https://as801.awmdm.com',
                 apikey='48RHTtFHt5NmujBLctmr4//29lhtngYEG+fkBE9ixFM=',
                 username='td.fuchs.marco',
                 password='yf$8@9Nv3J^N3#s6^#&84Y4s')
-
-gid = a.groups.get_id_from_groupid(groupid='tisp_fuchsm')
-print(gid)
