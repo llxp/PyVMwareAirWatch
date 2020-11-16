141441eb5b5bb014e50e2648c39b7a606f86ac26
Marco Fuchs <marco.fuchs@live.de>
2020-11-16 23:07:44 +0100

    Add support for .env files
    Added support for .env files to store credentials and keys in a separate file or os.environment variable

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from dotenv import load_dotenv
 from pyairwatch.client import AirWatchAPI
+import os
 import pprint
 
+# Load environment variables from .env
+load_dotenv()
+
+# Assign environment variables to CONSTANTS
+MY_API_KEY = os.getenv('MY_API_KEY')
+MY_USER = os.getenv('MY_USER')
+MY_PASS = os.getenv('MY_PASS')
+
+# Build the AirWatch API Client
 a = AirWatchAPI(env='https://as801.awmdm.com',
-                apikey='48RHTtFHt5NmujBLctmr4//29lhtngYEG+fkBE9ixFM=',
-                username='td.fuchs.marco',
-                password='yf$8@9Nv3J^N3#s6^#&84Y4s')
+                apikey=f'{MY_API_KEY}',
+                username=f'{MY_USER}',
+                password=f'{MY_PASS}')
+
+# Execute a system_info and pprint the output
+system_info = a.info.get_environment_info()
+pprint.pprint(system_info)
